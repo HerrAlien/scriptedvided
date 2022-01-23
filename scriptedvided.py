8f5da0459e196364611057fbcd466f97ae54ccab
Herr_Alien <alexandru.garofide@gmail.com>
2022-01-23 11:24:55 +0200

    some changes

--- scriptedvided.py
+++ scriptedvided.py
@@ -58,7 +58,7 @@ def truncate(input, start=-1, length=-1, output=None, recompress=False):
     
     return output
     
-def substituteAudio (inputVid, inputAudio, output=None, recompress=False):
+def substituteAudio (inputVid, inputAudio, output, recompress=False):
     params = ffmpegParams();
 
     params.append ("-vn")
@@ -80,7 +80,7 @@ def substituteAudio (inputVid, inputAudio, output=None, recompress=False):
     
     return output
     
-def overlayAudio (inputVid, inputAudio, output=None, firstStreamAudioWeight=0.1, recompressVideo=True):
+def overlayAudio (inputVid, inputAudio, output, firstStreamAudioWeight=0.1, recompressVideo=True):
     params = ffmpegParams();
 
     params = params + toInputParams(inputVid)    
@@ -116,14 +116,12 @@ def overlayAudio (inputVid, inputAudio, output=None, firstStreamAudioWeight=0.1,
     
     return output
 
-def append (firstStream, secondStream, output=None, recompressVideo=True):
+def append (firstStream, secondStream, output, recompressVideo=True):
     params = ffmpegParams();
 
-    params.append ("-i")
-    params.append(firstStream)    
+    params = params + toInputParams(firstStream)    
 
-    params.append ("-i")
-    params.append (secondStream)
+    params = params + toInputParams(secondStream)    
 
         
     params.append("-filter_complex")
@@ -135,7 +133,7 @@ def append (firstStream, secondStream, output=None, recompressVideo=True):
     
     if (output == None):
         secondRoot,ext = os.path.splitext (secondStream)
-        output = defaultOutput (firstStream, "_append_"  + os.path.basename(secondRoot))
+        output = defaultOutput ("", "_append_"  + os.path.basename(secondRoot))
 
     params.append(output)
     subprocess.run(params)
@@ -143,10 +141,12 @@ def append (firstStream, secondStream, output=None, recompressVideo=True):
     return output
     
 if __name__ == "__main__":
-   truncatedvid = truncate ( "C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", -10, 30, "vid.mp4" )
-   truncatedaudio = truncate ( "C:\\Users\\Admin\\Videos\\Generic old GPU advice.ogg", -10, 30, "audio.ogg" )
+#   truncatedvid = truncate ( "C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", -10, 30, "vid.mp4" )
+#   truncatedaudio = truncate ( "C:\\Users\\Admin\\Videos\\Generic old GPU advice.ogg", -10, 30, "audio.ogg" )
 #   truncatedaudio2 = truncate ("C:\\Users\\Admin\\Videos\\Generic old GPU advice.ogg", None, -2, 30)
-   overlayAudio (truncatedvid, truncatedaudio , "merged_audio.mp4", 0.15)
-   truncatedvid1 = truncate ("C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "t1.mp4", -40, 10)
-   truncatedvid2 = truncate ("C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "t2.mp4", -10, 10)
-   append(truncatedvid1, truncatedvid2, "appended.mp4")
+   overlayAudio ({"file":"C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "start" : -10, "length" : 30}, \
+   {"file":"C:\\Users\\Admin\\Videos\\Generic old GPU advice.ogg", "start":-10, "length" : 30} , "merged_audio.mp4", 0.15)
+#   truncatedvid1 = truncate ("C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "t1.mp4", -40, 10)
+#   truncatedvid2 = truncate ("C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "t2.mp4", -10, 10)
+   append({"file":"C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "start" : -10, "length" : 10}, \
+   {"file":"C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "start" : -40, "length" : 10}, "appended.mp4")
