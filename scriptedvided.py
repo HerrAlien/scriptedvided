9230f65c90e8e014eb5c0750a9da9bcbb89fe00d
Herr_Alien <alexandru.garofide@gmail.com>
2022-01-25 15:05:16 +0200

    transition of text

--- scriptedvided.py
+++ scriptedvided.py
@@ -39,6 +39,42 @@ def toInputParams (inputStream):
 
     return params
 
+    
+def getDrawTextCommand (text):
+    textSize = 24
+    paramText = ""
+    borderWidth = 0.5 * textSize
+    boxcolor="#00800080"
+#    transition=":enable=gt(t\,2)"
+#    transition=":x=if(gt(t\,3)\,0\,\(t-3\)*tw/3)"
+    transition=":x=if(gt(t\,4)\, "+ str(borderWidth) + "\,\(t-4\)*\(tw + " + str(borderWidth) + "\)/2)"
+    
+    if (type(text) is type("")):
+        paramText = "drawtext=y=H:box=1:boxborderw=" + str(borderWidth) +":boxcolor="+boxcolor+":font=Arial:fontsize=" + str (textSize) + ":fontcolor=White" + ":text="+text
+        paramText = paramText + transition
+    else:
+        lineSpacing = 0.5 * textSize
+        stepSize = (textSize + 2 * borderWidth + lineSpacing)
+        offsetFromBottom = len(text) * stepSize
+        step = 0
+        for line in text:
+            iterationStartNode = ";[intermediate"+ str(step) +"]"
+            iterationEndNode = "[intermediate"+ str(step + 1) +"]"
+            
+            if step == 0:
+                iterationStartNode = ""
+            
+            if (step == (len(text) - 1)):
+                iterationEndNode = ""
+
+            paramText = paramText  + iterationStartNode + "drawtext=y=H-"+ str(offsetFromBottom) +":box=1:boxborderw="+ str(borderWidth) + ":boxcolor="+boxcolor+":font=Arial:fontsize="+ str (textSize) +":fontcolor=White" + ":text="+line 
+            paramText = paramText + transition
+            paramText = paramText + iterationEndNode
+
+            offsetFromBottom = offsetFromBottom - stepSize
+            step=step+1
+
+    return paramText
 
 # input - the input media to be truncated
 # start - the input media to be truncated
@@ -176,9 +212,9 @@ def drawText (stream, text, output=None):
     params = ffmpegParams();
 
     params = params + toInputParams(stream)    
-    
     params.append("-filter_complex")
-    params.append("drawtext=fix_bounds=1:y=H:box=1:boxborderw=16:boxcolor=#008000:fontsize=24:fontcolor=White" + ":text="+text)
+
+    params.append(getDrawTextCommand(text))
     
     if (output == None):
         secondRoot,ext = os.path.splitext (stream)
@@ -192,7 +228,14 @@ def drawText (stream, text, output=None):
     
 def makeEpisodeFromFiles (video, audio, \
 textLinesArray=[], \
-options={"padAudio": 1, "padInserText" : 2, "videoSoundVolume" : 0.1}):
+options={"padAudio": 1, "padInsertText" : 2, "videoSoundVolume" : 0.1}):
+# pad the audio with 1 second of silence
+
+# video length must be larger than the audio. Loop it if needed.
+
+# then  overlay the audio
+
+# then print the text
     videoLen = getLengthOfStream(video)
     audioLen = getLengthOfStream(audio)
     padding = 1
@@ -222,4 +265,4 @@ if __name__ == "__main__":
 #   append({"file":"C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "start" : -10, "length" : 10}, \
 #   {"file":"C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4", "start" : -40, "length" : 10}, "appended.mp4")
 #    print(getLengthOfStream ("C:\\Users\\Admin\\Videos\\hd7770\\hd7770_RainbowSix_720p_100renderScale.mp4"))
-    drawText ("merged_audio.mp4", "'Rainbow 6 Siege (720p, low settings, render scale - 100)\nAverage - 73fps, 1 per cent lows - 32fps'")
+    drawText ("merged_audio.mp4", ["'Rainbow 6 Siege (720p, low settings, render scale 100\\\%)'", "'Average\: 73fps, 1\\\% lows\: 32fps'"])
