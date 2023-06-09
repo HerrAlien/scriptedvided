8f2e2a977553b4432426637e1084a3763917e4fb
Herr_Alien <alexandru.garofide@gmail.com>
2023-06-09 22:53:11 +0300

    skeleton script

--- r7_250_2023.py
+++ r7_250_2023.py
@@ -1,6 +1,6 @@
 import scriptedvided
 
-configs = { "defaultAudioFile" : "hd7770_2023.ogg",\
+configs = { "defaultAudioFile" : "r7_250_2023.ogg",\
 "mediaFolder" : "F:\\Videos\\hd7770_2023", \
 "stockFolder" : "F:\\Videos\\stock",\
 "benchmarkFile" : "F:\\Videos\\hd7770_2023\\Benchmark_hd7770_2023.txt",\
@@ -19,8 +19,8 @@ configs = { "defaultAudioFile" : "hd7770_2023.ogg",\
 {"file" : "Guide You Home - Ferco and Andie - Free Background Music - Audio Library Release.ogg", "timestamps" : ("00:20", None ), "destinationTimestamp" : { "title": "Usefulness of an old (D)GPU", "until" : "Blooper" }}, \
 ], "volume" : 0.05 },\
 "episodes" : [],\
-"youtube" : {"title" : "Will an 11 years old Radeon play games in 2023?", \
-"description" : '''In this video we're revisiting the HD7770. 
+"youtube" : {"title" : "10 years old budget Radeon versus 2023 games", \
+"description" : '''In this video we're revisiting the R7 250. 
 As usual, we're using the Z230 workstation with an i7 4770 equivalent CPU and 32 GB of DDR3 running in dual channel at 1600MHz.''',\
 "links" : '''
 Track: Bliss Of Heaven — SOMM [Audio Library Release]
@@ -39,12 +39,11 @@ Watch: https://www.youtube.com/watch?v=16eNerPDIsU&t=0s
 Free Download / Stream: https://alplus.io/guide-you-home
 
 Iceberg Tech's review of UHD 730: https://www.youtube.com/watch?v=5xvRPxVMQ1k
-Our 2022 review of the HD 7770: https://youtu.be/4rEcNy2YC0I
-Repairing an MSI HD7770: https://youtu.be/eqpQWX8f4Nw
+Our 2022 review of the R7 250: https://youtu.be/61OYLI9ym3s
 
-TechPowerup entry: https://www.techpowerup.com/gpu-specs/msi-hd-7770-ghz-edition.b2295
+TechPowerup entry: https://www.techpowerup.com/gpu-specs/asus-r7-250.b2488
 ''', \
-"tags" : "AMD,ATI,Radeon,HD7770,HD 7770,GCN,GCN1,GCN 1",\
+"tags" : "AMD,ATI,Radeon,R7 250,Oland,Oland XT,GCN,GCN1,GCN 1",\
 "language" : "EN", \
 "Caption certification" : "None",\
 "recording date" : None,\
@@ -61,34 +60,34 @@ TechPowerup entry: https://www.techpowerup.com/gpu-specs/msi-hd-7770-ghz-edition
 ####################### intro ###############################
 
 configs["episodes"].append(\
-{ "title": "Reason for retesting",\
-"audio" : {"timestamps" : ("00:00", "00:20" ), "volume" : 0.999, "padding" : 0.25 },\
-"video" : "msi_hd7770_breel_shaky.mp4"\
+{ "title": "One more reason for retesting",\
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 0.25 },\
+"video" : {"file": "msi_hd7770_breel_shaky.mp4", "rotation" : 180}\
 })
 
 configs["episodes"].append(\
 { "title": "Test system",\
-"audio" : {"timestamps" : ("00:20" , "00:33"), "volume" : 0.999, "padding" : 0.25 },\
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 0.25 },\
 "video" : {"file" : "Z230_closed.mp4", "rotation" : 180}\
 })
 
 configs["episodes"].append(\
 { "title": "The GPU",\
-"audio" : {"timestamps" : ("00:33", "00:52" ), "volume" : 0.999, "padding" : 0.25 },\
-"video" : {"file" : "hd7770_heaven_gpuz.mp4"}\
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 0.25 },\
+"video" : {"file" : "R7_250_gpu_capabilities.mkv"}\
 })
 
 configs["episodes"].append(\
 { "title": "Cooling and thermals",\
-"audio" : {"timestamps" : ("00:52" , "01:18"), "volume" : 0.999, "padding" : 0.25 },\
-"video" : "msi hd 7770 - fan c_ontroller, intended placement.mp4"\
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 0.25 },\
+"video" : "r7_250.mp4"\
 })
 
 configs["episodes"].append(\
 { "title": "Thermals",\
 "isChapter" : False, \
-"audio" : {"timestamps" : ("01:18", "01:35"), "volume" : 0.999, "padding" : 0.25 },\
-"video" : "msi_hd7770_W_arframe_temps.mp4"\
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 0.25 },\
+"video" : "r7_250_Heaven.mkv"\
 })
 
 ####################### end of intro ###############################
@@ -99,52 +98,49 @@ configs["episodes"].append(\
 ####################### gaming section ###############################
 configs["episodes"].append(\
 { "title": "Alien: Isolation",\
-"audio" : {"timestamps" : ("01:35","01:59") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, ultra settings", \
     }\
 }, \
-"video" : "stock_Alien_Isolation_1080p.mp4"\
 })
 
 
 configs["episodes"].append(\
 { "title": "Apex Legends",\
-"audio" : {"timestamps" : ("01:59", "02:32" )  },\
+"audio" : {"timestamps" : ("00:00", "00:00" )  },\
 "overlay" : { \
     "text" : ["'Apex Legends'",\
-              scriptedvided.r6sText('1920x1080, low settings', 44,  28),\
-              scriptedvided.r6sText('1280x720, low settings', 70,  52),\
+              scriptedvided.r6sText('1920x1080, low settings', 35,  24),\
+              scriptedvided.r6sText('1280x720, low settings', 55,  37),\
     ]\
 }, \
 })
 
 configs["episodes"].append(\
 { "title": "CoD Warzone",\
-"audio" : {"timestamps" : ("02:32", "03:00" )  },\
+"audio" : {"timestamps" : ("00:00", "00:00" )  },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1280x720, low settings", \
     }\
 }, \
-"video" : "hd7770_ModernWarfare_2023.mp4" \
 })
 
 configs["episodes"].append(\
 { "title": "Battlefield V",\
-"audio" : {"timestamps" : ("03:00", "03:24.5" ) },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1280x720, low settings", \
     }\
 }, \
-"video" : "stock_bfv_tobruk_720pLow.mp4",\
 })
 
 configs["episodes"].append(\
 { "title": "Control",\
-"audio" : {"timestamps" : ("03:24.5", "03:44.5") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1280x720, low settings", \
@@ -154,31 +150,29 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Rainbow Six: Siege",\
-"audio" : {"timestamps" : ("03:44.5", "04:21") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "text" : ["'Rainbow Six\: Siege'",\
-              scriptedvided.r6sText('1920x1080, low settings, 100% render scale', 49, 40), \
-              scriptedvided.r6sText('1280x720, low settings, 100% render scale', 93, 64),\
-              scriptedvided.r6sText('1920x1080, low settings, 50% render scale', 67, 50),\
-              scriptedvided.r6sText('1280x720, low settings, 50% render scale', 121, 77)]\
+              scriptedvided.r6sText('1920x1080, low settings, 100% render scale', 34, 28), \
+              scriptedvided.r6sText('1280x720, low settings, 100% render scale', 69, 50),\
+              scriptedvided.r6sText('1920x1080, low settings, 50% render scale', 51, 37),\
+              scriptedvided.r6sText('1280x720, low settings, 50% render scale', 92, 61)]\
 }, \
 })
 
 configs["episodes"].append(\
 { "title": "Grand Theft Auto V",\
-"audio" : {"timestamps" : ("04:21",  "04:58") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
-    "text" : ["'Grand Theft Auto V'",\
-              scriptedvided.r6sText('1920x1080, low settings', 85,  59),\
-              scriptedvided.r6sText('1280x720, low settings', 117,  71),\
-    ]\
+    "benchmark" : { \
+        "settings" : "1920x1080, low settings", \
+    }\
 }, \
-"video" : "stock_gta5_1080p.mp4" \
 })
 
 configs["episodes"].append(\
 { "title": "Counter Strike: Global Offensive",\
-"audio" : {"timestamps" : ( "04:58" , "05:26" ) },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, low settings", \
@@ -188,7 +182,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "DOTA2",\
-"audio" : {"timestamps" : ("05:26", "05:50") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, low settings", \
@@ -198,7 +192,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Fortnite",\
-"audio" : {"timestamps" : ("05:50", "06:18") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, performance mode, view distance FAR, 100% render scale", \
@@ -208,42 +202,40 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Overwatch 2",\
-"audio" : {"timestamps" : ( "06:18", "06:50") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "text" : ["'Overwatch 2'",\
-              scriptedvided.r6sText('1920x1080, low settings', 76,  57),\
-              scriptedvided.r6sText('1600x900, low settings', 107,  81),\
-              scriptedvided.r6sText('1280x720, low settings', 152,  116),\
+              scriptedvided.r6sText('1920x1080, low settings', 55,  41),\
+              scriptedvided.r6sText('1600x900, low settings', 78,  61),\
+              scriptedvided.r6sText('1280x720, low settings', 114,  89),\
     ]\
 }, \
 })
 
 configs["episodes"].append(\
 { "title": "Rocket League",\
-"audio" : {"timestamps" : ("06:50", "07:05") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, low settings, 100% render scale", \
     }\
 }, \
-"video" : "stock_RocketLeague_1080pLower.mp4",\
 })
 
 configs["episodes"].append(\
 { "title": "Splitgate",\
-"audio" : {"timestamps" : ("07:05", "07:36") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "text" : ["'Splitgate'",\
-              scriptedvided.r6sText('1920x1080, low settings, 100% render scale', 71,  54),\
-              scriptedvided.r6sText('1280x720, low settings, 100% render scale', 120,  83),\
+              scriptedvided.r6sText('1920x1080, low settings, 100% render scale', 48,  30),\
+              scriptedvided.r6sText('1280x720, low settings, 100% render scale', 91,  67),\
     ]\
 }, \
-"video" : "stock_Splitgate_1080pLow.mp4",\
 })
 
 configs["episodes"].append(\
 { "title": "Valorant",\
-"audio" : {"timestamps" : ("07:36", "08:19") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, low settings", \
@@ -253,7 +245,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Genshin Impact",\
-"audio" : {"timestamps" : ("08:19", "08:57") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, low settings, 1.0 render scale", \
@@ -264,7 +256,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "HiRez Studios trio (Paladins, Realm Royale, Rogue Company)",\
-"audio" : {"timestamps" : ("08:57", "09:14" ), "volume" : 0.999, "padding" : 0.25 },\
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 0.25 },\
 "video" : {"file":"HiRezTrio-P_aladins_R_ealmRoyale_R_ogueCompany.mp4"}\
 })
 
@@ -273,7 +265,7 @@ configs["episodes"].append(\
 configs["episodes"].append(\
 { "title": "Paladins",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("09:14", "09:20.5") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, high settings", \
@@ -284,7 +276,7 @@ configs["episodes"].append(\
 configs["episodes"].append(\
 { "title": "Realm Royale",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("09:20.5", "09:26.5") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, high settings", \
@@ -295,7 +287,7 @@ configs["episodes"].append(\
 configs["episodes"].append(\
 { "title": "Rogue Company",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("09:26.5", "09:36.4") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, low settings", \
@@ -305,14 +297,14 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "trio close",\
-"audio" : {"timestamps" : ("09:36.4", "09:45") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "video" : {"file":"HiRezTrio-P_aladins_R_ealmRoyale_R_ogueCompany.mp4"},\
 "isChapter" : False,\
 })
 
 configs["episodes"].append(\
 { "title": "World of Tanks Blitz",\
-"audio" : {"timestamps" : ("09:45", "10:05") },\
+"audio" : {"timestamps" :("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, high settings", \
@@ -322,7 +314,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Warframe",\
-"audio" : {"timestamps" : ("10:05", "10:30") },\
+"audio" : {"timestamps" : ("00:00", "00:00" ) },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1920x1080, low settings", \
@@ -342,32 +334,39 @@ configs["episodes"].append(\
 ###########################
 
 configs["episodes"].append(\
-{ "title": "Is the HD 7770 worth it?",\
-"audio" : {"timestamps" : (  "10:30", "10:54"), "volume" : 0.999 },\
+{ "title": "How much is the R7 250 worth?",\
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999 },\
+"video" : {"file" : ""}\
+})
+
+configs["episodes"].append(\
+{ "title": "HD7770 as rival",\
+"isChapter" : False,\
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999 },\
 "video" : {"file" : "hd7770_olxSearch.mkv"}\
 })
 
 
 configs["episodes"].append(\
-{ "title": "rx470",\
+{ "title": "HD 7750 preview",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("10:54", "11:18"), "volume" : 0.999, "padding" : 0.25  },\
-"video" : {"file" : "rx470_olxSearch.mkv"} \
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 0.25  },\
+"video" : {"file" : ""} \
 })
 
 
 configs["episodes"].append(\
 { "title": "bye",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ( "11:18", "11:26.5"), "volume" : 0.999, "padding" : 1  },\
-"video" : {"file" : "msi_hd7770_breel_grass.mp4"} \
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 1  },\
+"video" : {"file" : "r7_250_broll_boxPortrait.mp4"} \
 })
 
 configs["episodes"].append(\
 { "title": "Blooper",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("11:26.5", "11:40"), "volume" : 0.999, "padding" : 0.25  },\
-"video" : {"file" : "msi_hd7770_breel_dog.mp4"} \
+"audio" : {"timestamps" : ("00:00", "00:00" ), "volume" : 0.999, "padding" : 0.25  },\
+"video" : {"file" : "r7_250_broll_dog.mp4"} \
 })
 
 
