f848bae2e0bc756d8b428f8555969a9776c8e765
Herr_Alien <alexandru.garofide@gmail.com>
2023-10-04 22:39:07 +0300

    initial version R7 26N

--- cs2_r7_26n.py
+++ cs2_r7_26n.py
@@ -1,14 +1,14 @@
 import scriptedvided
 
-configs = { "defaultAudioFile" : "CapeVerde_CS2.ogg",\
-"mediaFolder" : "F:\\Videos\\CapeVerde_CS2", \
+configs = { "defaultAudioFile" : "R7_26N_CS2.ogg",\
+"mediaFolder" : "F:\\Videos\\R7_26N_CS2", \
 "stockFolder" : "F:\\Videos\\stock",\
-"outputFolder" : "F:\\Videos\\CapeVerde_CS2\\output", \
-"outputFile" : "CapeVerde_CS2.mp4", \
+"outputFolder" : "F:\\Videos\\R7_26N_CS2\\output", \
+"outputFile" : "R7_26N_CS2.mp4", \
 "textOpts" : {"fontcolor" : "White", "boxcolor" : "#80000080"},\
 "backgroundTrack" : { "audioTracks" : [ \
 {"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3",\
-"timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "Not all 1GB cards are the same", "until" : "1920x1080 results"}}, \
+"timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "60 FPS at 1080?", "until" : "1920x1080 results"}}, \
 {"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3",\
 "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "1920x1080 results", "until" : "Conclusion"}}, \
 {"file" : "Guide You Home - Ferco and Andie - Free Background Music - Audio Library Release.ogg", "timestamps" : ("00:20", None ), "destinationTimestamp" : { "title": "Conclusion", "until" : "Blooper" }}, \
@@ -36,7 +36,7 @@ Free Download / Stream: https://alplus.io/guide-you-home
 More games tested with the TeraScale 2 cards:
 https://www.youtube.com/watch?v=9ZrKAwA6MbQ&list=PLgBGV4K3p2_YhzgbzcQ6kdgfGpzdq6cRD
 ''', \
-"tags" : "Counter Strike 2,CS2,Radeon,AMD,1GB,SM5,GCN,GCN 1.0,R7 250,HD7750,HD 7750,HD7770,HD 7770,R7 250E,R7 250 512SP,R7 250X,60 FPS,60FPS",\
+"tags" : "Counter Strike 2,CS2,Radeon,AMD,1GB,2GB,SM5,GCN,GCN 1.0,GCN 2.0,GCN2,R7 260,HD7790,HD 7790,R7 260X,HD7850,HD 7850,R7 265,R7 370,MSI,ASUS,Sapphire,60 FPS,60FPS",\
 "language" : "EN", \
 "Caption certification" : "None",\
 "recording date" : None,\
@@ -50,52 +50,52 @@ https://www.youtube.com/watch?v=9ZrKAwA6MbQ&list=PLgBGV4K3p2_YhzgbzcQ6kdgfGpzdq6
 
 
 configs["episodes"].append(\
-{ "title": "Not all 1GB cards are the same",\
-"audio" : {"timestamps" : ("00:00", "00:14" ), "volume" : 0.999, "padAudio" : 0.25 },\
+{ "title": "60 FPS at 1080?",\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
 "video" : {"file" : "TS2_CS2.mp4", "start" : "01:10"},\
 })
 
 # this will be split up
 configs["episodes"].append(\
-{ "title": "Meet the GPUs and test system (again)",\
-"audio" : {"timestamps" : ("00:14", "00:25.7" ), "volume" : 0.999, "padAudio" : 0.25 },\
-"video" : {"file" : "stock_CapeVerde_family.mp4", "start" : "00:00", "rotation" : 90}\
+{ "title": "The GPUs and test system",\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "stock_midGCNs_family.mp4", "start" : "00:00", "rotation" : 180}\
 })
 
 configs["episodes"].append(\
-{ "title": "R7 250",\
+{ "title": "R7 260",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("00:25.7", "00:34" ), "volume" : 0.999, "padAudio" : 0.25 },\
-"video" : {"file" : "stock_CapeVerde_family.mp4", "start" : "02:24", "rotation" : 90}\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "stock_midGCNs_family.mp4", "start" : "00:40", "rotation" : 180}\
 })
 
 # 7770 - 51, 7750 - 01:40 
 
 configs["episodes"].append(\
-{ "title": "HD 7750",\
+{ "title": "R7 260X",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("00:34", "00:49" ), "volume" : 0.999, "padAudio" : 0.25 },\
-"video" : {"file" : "stock_CapeVerde_family.mp4", "start" : "01:40", "rotation" : 90}\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "stock_midGCNs_family.mp4", "start" : "01:05", "rotation" : 180}\
 })
 
 configs["episodes"].append(\
-{ "title": "HD 7770",\
+{ "title": "R7 265",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("00:49", "00:57" ), "volume" : 0.999, "padAudio" : 0.25 },\
-"video" : {"file" : "stock_CapeVerde_family.mp4", "start" : "00:51", "rotation" : 90}\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "stock_midGCNs_family.mp4", "start" : "01:38", "rotation" : 180}\
 })
 
 configs["episodes"].append(\
 { "title": "Z230",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("00:57", "01:13" ), "volume" : 0.999, "padAudio" : 0.25 },\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
 "video" : {"file" : "Z230_closed.mp4" , "rotation" : 180}\
 })
 
 configs["episodes"].append(\
 { "title": "1920x1080 results",\
-"audio" : {"timestamps" : ("01:13", "02:14" )  },\
-"video" : {"file": "stock_cs2_T_R7_250.mp4"},\
+"audio" : {"timestamps" : ("", "") , "padAudio" : 0.25 },\
+"video" : {"file": "stock_CS2_CT_R7_260.mp4"},\
 "overlay" : { \
     "image" : {"file" : "1080, low settings.png", "chromaColor" : "0x00FF00"}\
 }, \
@@ -103,8 +103,8 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "1280x720 results",\
-"audio" : {"timestamps" : ("02:14", "03:08" )  },\
-"video" : {"file": "stock_cs2_CT_hd7770.mp4"},\
+"audio" : {"timestamps" : ("", ""), "padAudio" : 0.25 },\
+"video" : {"file": "stock_CS2_T_R7_265.mp4"},\
 "overlay" : { \
     "image" : {"file" : "720, low settings.png", "chromaColor" : "0x00FF00"}\
 }, \
@@ -112,36 +112,36 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Conclusion",\
-"audio" : {"timestamps" : ("03:08", "03:26.33"), "volume" : 0.999 , "padAudio" : 0.25 },\
-"video" : {"file" : "stock_CapeVerde_family.mp4", "start" : "00:43", "rotation" : 90}\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "stock_midGCNs_family.mp4", "start" : "00:00", "rotation" : 180}\
 })
 
 configs["episodes"].append(\
-{ "title": "7770",\
+{ "title": "win",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("03:26.33", "03:43"), "volume" : 0.999 , "padAudio" : 0.25 },\
-"video" : {"file" : "stock_CapeVerde_family.mp4", "start" : "00:56", "rotation" : 90}\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "stock_CS2_CT_R7_260.mp4", "start" : "07:13"}\
 })
 
 configs["episodes"].append(\
-{ "title": "no 1080",\
+{ "title": "High end GCN",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("03:43", "04:03"), "volume" : 0.999 , "padAudio" : 0.25 },\
-"video" : {"file" : "stock_cs2_T_gt1030lp.mp4"},\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "breel_r9_290x_outside_dog.mp4", "rotation" : 180, "start" : "00:20"},\
 })
 
 configs["episodes"].append(\
-{ "title": "GCN family - mid level",\
+{ "title": "End of the series",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("04:03", "04:13.5"), "volume" : 0.999 , "padAudio" : 0.25 },\
-"video" : {"file" : "stock_midGCNs_family.mp4", "start" : "00:00" , "rotation" : 0}\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "stock_CS2_CT_R7_260.mp4", "start" : "00:00" , "rotation" : 0}\
 })
 
 configs["episodes"].append(\
 { "title": "Bye",\
 "isChapter" : False,\
-"audio" : {"timestamps" : ("04:13.5", "04:21"), "volume" : 0.999 , "padAudio" : 0.25 },\
-"video" : {"file" : "stock_CapeVerde_family.mp4", "start" : "00:00", "rotation" : 90}\
+"audio" : {"timestamps" : ("", ""), "volume" : 0.999 , "padAudio" : 0.25 },\
+"video" : {"file" : "stock_midGCNs_family.mp4", "start" : "00:00", "rotation" : 180}\
 })
 
 
