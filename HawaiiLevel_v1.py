1bb6f6aeff8bd782e15768529657dc4a239fee58
Herr_Alien <alexandru.garofide@gmail.com>
2024-04-29 23:33:28 +0300

    skeleton

---
+++ HawaiiLevel_v1.py
@@ -0,0 +1,329 @@
+import scriptedvided
+
+configs = { "defaultAudioFile" : "HawaiiLevel_v1.ogg",\
+"mediaFolder" : "F:\\Videos\\HawaiiLevel_v1", \
+"stockFolder" : "F:\\Videos\\stock",\
+"outputFolder" : "F:\\Videos\\HawaiiLevel_v1\\output", \
+"outputFile" : "HawaiiLevel_v1.mp4", \
+"benchmarkFile" : "",\
+"textOpts" : {"fontcolor" : "White", "boxcolor" : "#00800080"},\
+"backgroundTrack" : { "audioTracks" : [ \
+{"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "I (heart) Maxwell", "until" : "Alien Isolation"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Alien Isolation", "until" : "Counter-Strike 2"}}, \
+{"file" : "Inspired - MaikonMusic  Free Background Music  Audio Library Release.mp3", "timestamps" : ("00:00", None ), "destinationTimestamp" : {"title" : "Counter-Strike 2", "until" : "Overwatch 2"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Overwatch 2", "until" : "Again, (heart) Maxwell ..."}}, \
+{"file" : "Guide You Home - Ferco and Andie - Free Background Music - Audio Library Release.ogg", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "Again, (heart) Maxwell ...", "until" : "Blooper"}}, \
+], "volume" : 0.045 },\
+"episodes" : [],\
+"youtube" : {"title" : "  ...  ... ", \
+"description" : '''In this one we're testing the 4(?)GB GTX 970 using some games from today.''',\
+"links" : '''
+Track: Bliss Of Heaven - SOMM [Audio Library Release]
+Music provided by Audio Library Plus
+Watch: https://www.youtube.com/watch?v=JQ6mKeQLZak&t=0s
+Free Download / Stream: https://alplus.io/blisss-heaven
+
+Track: Far Far Away - Ferco [Audio Library Release]
+Music provided by Audio Library Plus
+Watch: https://www.youtube.com/watch?v=SrkQ3K1umlc&t=0s 
+Free Download / Stream: https://alplus.io/far-far-away
+
+Track: Inspired - MaikonMusic [Audio Library Release]
+Music provided by Audio Library Plus
+Watch: https://www.youtube.com/watch?v=RUkdTkk_52o&t=0s
+Free Download / Stream: https://alplus.io/inspired
+
+Track: Guide You Home - Ferco & Andie [Audio Library Release]
+Music provided by Audio Library Plus
+Watch: https://www.youtube.com/watch?v=16eNerPDIsU&t=0s
+Free Download / Stream: https://alplus.io/guide-you-home
+
+''', \
+"tags" : "NVidia,GeForce,GTX,GTX970,GTX 970,Maxwell,Maxwell 2.0,AMD,Radeon,GCN,Polaris,R9 290X,RX570,RX 570",\
+"language" : "EN", \
+"Caption certification" : "None",\
+"recording date" : None,\
+"video location" : None, \
+"category" : "Gaming", \
+"subtitles" : None, \
+"endscreen" : None, \
+"cards" : None, \
+}\
+}
+
+lastTS = "00:00"
+configs["episodes"].append(\
+{ "title": "Taking my own advice",\
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "This is for single player",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "What is good enough",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Meet the hardware",\
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "R9 290X",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "GTX 970",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "RX570",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Z230",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"overlay" : { \
+    "text" : ["'Z230 workstation from HP'",\
+              "'CPU\: E3-1241v3 Xeon (i7 4770 equivalent)'",\
+              "'RAM\: 32GB DDR3 at 1600 MHz, dual channel'",\
+    ]\
+}, \
+"video" : {"file" : "Z230_closed.mp4", "rotation" : 180}\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+#########################################
+
+
+configs["episodes"].append(\
+{ "title": "Control - high settings",\
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+#needs overlay
+configs["episodes"].append(\
+{ "title": "Control - high - GTX 970",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "Control-high.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Control - high - R9 290X",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "Control-high.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Control - high - RX570",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "Control-high.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Control - medium settings",\
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Control - medium - all",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "Control-medium.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Control - conclusion",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Fallout 4 - ultra",\
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Fallout 4 - high vs medium",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Fallout 4 - ultra results",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "Fallout4-ultra.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Fallout 4 - high",\
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "Fallout4-high.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Fallout 4 - conclusion",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "The exception to 30 FPS 1% lows",\
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Doom Eternal - not higher than high",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Doom Eternal - high",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "DoomEternal-high.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Doom Eternal - conclusions",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+# average FPS
+configs["episodes"].append(\
+{ "title": "Conclusions",\
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "sum-averages.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Conclusions one percent",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+"overlay" : { \
+    "image" : {"file" : "sum-one-percent.png", "chromaColor" : "0x00FF00"}\
+}, \
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Other games",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+configs["episodes"].append(\
+{ "title": "Bye",\
+"isChapter" : False, \
+"audio" : {"timestamps" : (lastTS, "" ), "volume" : 0.999, "padAudio" : 0.1 },\
+"video" : {"file" : "" },\
+})
+lastTS = configs["episodes"][-1]["audio"]["timestamps"][1]
+
+
+
+#scriptedvided.makeVideoForEpisode(configs["episodes"][1], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][4], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][11], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][12], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][13], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][8], configs)
+#print(scriptedvided.makeVideoForEpisode(configs["episodes"][9], configs))
+#print(scriptedvided.getSuitableVideoStream(configs["episodes"][9], configs))
+#print (configs["youtube"])
+#print(scriptedvided.getMusicCreditsString(configs["backgroundTrack"]))
+#scriptedvided.makeVideoForEpisode([x for x in configs["episodes"] if x["title"] == "Alien Isolation"][0], configs)
+#scriptedvided.makeVideoForEpisode([x for x in configs["episodes"] if x["title"] == "actual 1080 results"][0], configs)
+#scriptedvided.makeVideoForEpisode([x for x in configs["episodes"] if x["title"] == "actual 900 results"][0], configs)
+#scriptedvided.makeVideoForEpisode([x for x in configs["episodes"] if x["title"] == "actual 720 results"][0], configs)
+#print (scriptedvided.getSuitableImage([x for x in configs["episodes"] if x["title"] == "actual 1080 results"][0], configs))
+
+scriptedvided.makeVideo(configs)
+
+# meeds better video, or maybe break it up
