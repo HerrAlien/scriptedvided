53c1a73c2c4fcf96a77bbdb29e3b3ae7a6ce9d0a
Herr_Alien <alexandru.garofide@gmail.com>
2024-12-29 08:11:54 +0200

    fixed syntax error

--- test_system_r5_3600.py
+++ test_system_r5_3600.py
@@ -194,7 +194,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "same company as the case",\
-"audio" : {"timestamps" : "04:11.3", "04:20.7" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : ("04:11.3", "04:20.7" ), "volume" : 0.999, "padAudio" : 0.05 },\
 "isChapter" : False,\
 "video" : {"file" : "system_aigo_sameManufacturer.mp4", "start" : "00:11"},\
 })
