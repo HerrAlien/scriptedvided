cb6b65a704c6d7fb782aea94f7a1c3128f2222bd
Hindsight_Lad <alexandru.garofide@siemens.com>
2025-07-01 16:41:08 +0300

    started with the 2400

---
+++ i5_2400_2025.py
@@ -0,0 +1,419 @@
+import scriptedvided
+
+configs = { "defaultAudioFile" : "i7_2600.ogg",\
+"mediaFolder" : "F:\\Videos\\i7_2600", \
+"stockFolder" : "F:\\Videos\\stock",\
+"benchmarkFile" : "F:\\Videos\\i7_2600\\Benchmark_i7_2600.txt",\
+"outputFolder" : "F:\\Videos\\i7_2600\\output", \
+"outputFile" : "i7_2600.mp4", \
+"textOpts" : {"fontcolor" : "White", "boxcolor" : "#00008080"},\
+"backgroundTrack" : { "audioTracks" : [ \
+{"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "The OG 2600", "until" : "Marvel Rivals"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Marvel Rivals", "until" : "Shadow of the Tomb Raider"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Shadow of the Tomb Raider", "until" : "Video rendering test (The GigASUS)"}}, \
+{"file" : "Ferco - Inquisitiveness.ogg", "timestamps" : ("01:01", None ), "destinationTimestamp" : {"title" : "Video rendering test (The GigASUS)", "until" : "Conclusions"}}, \
+{"file" : "Inspired - MaikonMusic  Free Background Music  Audio Library Release.mp3", "timestamps" : ("00:00", None ), "destinationTimestamp" : {"title" : "Conclusions", "until" : "EOF"}}, \
+], "volume" : 0.05 },\
+"episodes" : [],\
+"youtube" : {"title" : "", \
+"description" : '''''',\
+"links" : '''
+Track: Bliss Of Heaven - SOMM [Audio Library Release]
+Music provided by Audio Library Plus
+Watch: https://www.youtube.com/watch?v=JQ6mKeQLZak&t=0s
+Free Download / Stream: https://alplus.io/blisss-heaven
+
+Track: Far Far Away - Ferco [Audio Library Release]
+Music provided by Audio Library Plus
+Watch: https://www.youtube.com/watch?v=SrkQ3K1umlc&t=0s 
+Free Download / Stream: https://alplus.io/far-far-away
+
+Track: Ferco - Inquisitiveness
+Creative Commons - Attribution 3.0 Unported (CC BY 3.0)
+Free Download: https://hypeddit.com/mlsvxq
+Streams: https://share.amuse.io/track/ferco-inquisitiveness
+Video: https://www.youtube.com/watch?v=dhJdmwLWtFM&t=0s
+
+
+Track: Inspired - MaikonMusic [Audio Library Release]
+Music provided by Audio Library Plus
+Watch: https://www.youtube.com/watch?v=RUkdTkk_52o&t=0s
+Free Download / Stream: https://alplus.io/inspired
+
+''', \
+"tags" : "",\
+"language" : "EN", \
+"Caption certification" : "None",\
+"recording date" : None,\
+"video location" : None, \
+"category" : "Gaming", \
+"subtitles" : None, \
+"endscreen" : None, \
+"cards" : None, \
+}\
+}
+
+#"isChapter" : False, \
+
+####################### intro ###############################
+
+# this is the hook 
+#  scriptedvided.nextTS\(configs\)\, *\"[0-9][0-9]\:[0-9][0-9]\.?[0-9]?[0-9]?\"
+#  \"file\" *\: *\".*\"
+#  scriptedvided.r6sText\('.*' *\, *[0-9]*\, *[0-9]*\)
+#   "timestamps" *\: *\( *"[0-9\:\.]*" *\, *"[0-9\:\.]*" *\)
+#configs["episodes"].append(\
+#{ "title": "A favorite of miners and gamers alike",\
+#"audio" : {"timestamps" : (scriptedvided.nextTS(configs),  "" ), "volume" : 0.999, "padAudio" : 0.05 },\
+#"video" : {"file" : ""},\
+#})
+
+configs["episodes"].append(\
+{ "title": "The OG 2600",\
+"audio" : {"timestamps" : ("00:00", "00:14.6"), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "bared_i7_2600.mp4"},\
+})
+
+configs["episodes"].append(\
+{ "title": "inspectre",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:19.1" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "inspectre_off.mp4"},\
+})
+
+
+configs["episodes"].append(\
+{ "title": "The CPU",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:34.2" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "i7_2600_cores_tdp.mkv"},\
+})
+
+# needs a side by side video
+configs["episodes"].append(\
+{ "title": "The TDP is real",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:51.4" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "i7_2600_realTDP.mp4"},\
+})
+
+configs["episodes"].append(\
+{ "title": "The games and test system",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:04.5" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"overlay" : { \
+    "text" : ["'Custom build'",\
+              "'CPU\: Core i7 2600'",\
+              "'RAM\: 16GB DDR3, 1600MHz, dual channel'",\
+              "'GPU\: EVGA GTX 970'",\
+    ]\
+}, \
+"video" : {"file" : "breel_GA-B75-D3V_i7_2600_case.mp4" }\
+})
+
+configs["episodes"].append(\
+{ "title": "case modding",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:19.4" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "case_modding_before_after.mp4"},\
+})
+
+configs["episodes"].append(\
+{ "title": "GPU",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:29.8" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "breel_autumn_GTX970.mp4"},\
+})
+
+
+configs["episodes"].append(\
+{ "title": "Selected games",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:38.3" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "i7_2600_cpu_usage_M_arvelRivals.mp4"},\
+})
+
+configs["episodes"].append(\
+{ "title": "Selected games sp",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:44" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "i7_2600_cpu_usage_S_O_TTR.mp4"},\
+})
+
+configs["episodes"].append(\
+{ "title": "Gigasus test",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:51.4" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "Gigasus_ffwd10.mp4"},\
+})
+####################### end of intro ###############################
+
+
+####################### gaming section ###############################
+configs["episodes"].append(\
+{ "title": "Marvel Rivals",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:10.7" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "1280x720, 30% render scale, low settings", \
+    }\
+}, \
+"video" : {"file" : "Marvel-Win64-Shipping_2025_03_09_23_32_45_673.mp4"},\
+})
+
+# needs redone
+configs["episodes"].append(\
+{ "title": "Cyberpunk 2077",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:34.2" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "1280x720, FSR 2.1 performance, low settings", \
+    }\
+}, \
+})
+
+configs["episodes"].append(\
+{ "title": "Fortnite",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:05.6" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "text" : ["'Fortnite'",\
+              scriptedvided.r6sText('1280x720, performance mode', 133 , 54 ),\
+              scriptedvided.r6sText('1280x720, performance mode, inSpectre', 135 , 67)\
+]}, \
+"video" : "Fortnite_Win_2025-04-21.mkv"\
+})
+
+configs["episodes"].append(\
+{ "title": "Rainbow Six: Siege",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:44.8"), "padAudio" : 0.05 },\
+"overlay" : { \
+    "text" : ["'Rainbow Six\: Siege'",\
+              scriptedvided.r6sText('1280x720, 50% scale, low settings', 160 , 127),\
+              scriptedvided.r6sText('1280x720, 50% scale, low settings, inSpectre', 172 , 134)\
+]}, \
+})
+
+configs["episodes"].append(\
+{ "title": "Apex Legends",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:04.2" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "1280x720, low settings", \
+    }\
+},
+"video" : "rx580_r5apex_2024_07_26_22_34_08_382-converted.mp4" \
+})
+
+configs["episodes"].append(\
+{ "title": "Overwatch 2",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:28" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "1280x720, low settings", \
+    }\
+}\
+})
+
+configs["episodes"].append(\
+{ "title": "Counter-Strike 2",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:04.8" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "text" : ["'Counter-Strike 2'",\
+              scriptedvided.r6sText('1280x720, low settings', 87 , 53),\
+              scriptedvided.r6sText('1280x720, low settings, inSpectre', 92, 60)\
+]},\
+})
+
+configs["episodes"].append(\
+{ "title": "DOTA 2",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:21" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "1920x1080, low settings", \
+    }\
+},\
+"video" : "stock_dota2_1080pLow_scale100.mp4"\
+})
+
+configs["episodes"].append(\
+{ "title": "The Finals",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:43.8" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "1280x720, low settings", \
+    }\
+},\
+})
+
+#redo and specify a smoother video
+configs["episodes"].append(\
+{ "title": "Shadow of the Tomb Raider",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:00.3" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "800x600, low settings", \
+    }\
+},\
+"video" : {"file" : "SOTTR_gaming_highest.mp4", "start" : "04:53"}\
+})
+
+configs["episodes"].append(\
+{ "title": "Borderlands 3",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:28" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "1280x720, 50% render scale, lowest settings", \
+    }\
+},\
+})
+
+configs["episodes"].append(\
+{ "title": "Control",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:50.9" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "960x540, lowest settings", \
+    }\
+},\
+})
+
+configs["episodes"].append(\
+{ "title": "Doom Eternal",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "07:21.9" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "text" : ["'Doom Eternal - lol, nope ...'"]},\
+})
+
+####################### end of gaming section ###############################
+
+
+############################ productivity #################################
+
+# this is both time and cycles, for both SMT on and off
+configs["episodes"].append(\
+{ "title": "Video rendering test (The GigASUS)",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "07:26.4" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "Gigasus_ffwd10.mp4", "start" : "00:00"},\
+})
+
+configs["episodes"].append(\
+{ "title": "HT on, no inSpectre",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "07:47.6" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "blurred_dimmed_gigasus.mp4", "start" : "00:30"},\
+"isChapter" : False,\
+"overlay" : { \
+    "image" : {"file" : "Clock Cycles stock.png"}\
+}, \
+})
+
+configs["episodes"].append(\
+{ "title": "HT on, w inSpectre",\
+"audio" : {"timestamps" : ("07:48.3", "08:02.3" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "blurred_dimmed_gigasus.mp4", "start" : "01:00"},\
+"isChapter" : False,\
+"overlay" : { \
+    "image" : {"file" : "Clock Cycles w inspectre.png"}\
+}, \
+})
+
+configs["episodes"].append(\
+{ "title": "HT off, no inSpectre",\
+"audio" : {"timestamps" : ("08:03.9", "08:23.6" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "blurred_dimmed_gigasus.mp4", "start" : "00:00"},\
+"isChapter" : False,\
+"overlay" : { \
+    "image" : {"file" : "Clock Cycles HT.png"}\
+}, \
+})
+
+configs["episodes"].append(\
+{ "title": "HT vs power consumption",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "08:31.8" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "blurred_dimmed_gigasus.mp4", "start" : "00:30"},\
+"isChapter" : False,\
+"overlay" : { \
+    "image" : {"file" : "Clock Cycles HT and power.png"}\
+}, \
+})
+
+configs["episodes"].append(\
+{ "title": "IPC compared to other CPUs, spectre and meltdown",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "08:41.4" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "blurred_dimmed_gigasus.mp4", "start" : "01:00"},\
+"isChapter" : False,\
+"overlay" : { \
+    "image" : {"file" : "Clock Cycles w inspectre.png"}\
+}, \
+})
+
+configs["episodes"].append(\
+{ "title": "cs2, r6s and ",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "08:49.1" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "stock_CS2_CT_R7_260X.mp4"},\
+"isChapter" : False,\
+})
+
+configs["episodes"].append(\
+{ "title": "Final warning",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "08:59.3" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "rx580_Discovery_2024_07_26_23_03_56_417-converted.mp4"},\
+"isChapter" : False,\
+})
+
+#############################################################################
+
+
+
+####################### conclusion ###############################
+# similar ro 4th gen i7
+configs["episodes"].append(\
+{ "title": "Conclusions",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "09:06.2" ),  "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "bared_i7_2600.mp4"},\
+})
+
+configs["episodes"].append(\
+{ "title": "with i5 2400",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "09:14.4" ),  "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "olx_i7_2600_i5_2400.mkv"},\
+})
+
+configs["episodes"].append(\
+{ "title": "2400 alone",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "09:21.6" ),  "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : "bared__i7_2600_i5_2500.mp4" \
+})
+
+configs["episodes"].append(\
+{ "title": "bye",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs),  "09:31.6" ),  "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "bared_i7_2600.mp4"},\
+})
+
+
+##configs["episodes"].append(\
+##{ "title": "Blooper",\
+##"isChapter" : False,\
+##"audio" : {"timestamps" : (scriptedvided.nextTS(configs),  "" ) },\
+##"video" : {"file" : ""}\
+##})
+
+#scriptedvided.makeVideoForEpisode(configs["episodes"][8], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][9], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][13], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][15], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][16], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][17], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][18], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][19], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][21], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][22], configs)
+#scriptedvided.makeVideoForEpisode(configs["episodes"][24], configs)
+#scriptedvided.makeVideoForEpisode([x for x in configs["episodes"] if x["title"] == "Borderlands 3"][0], configs)
+#scriptedvided.makeVideo(configs)
+
+#for x in range(19,26):
+#    scriptedvided.makeVideoForEpisode(configs["episodes"][x], configs)
+#
+scriptedvided.makeVideo(configs)
+
