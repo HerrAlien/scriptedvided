1b109f743c26e4221f3fb26819cfbfc27313800f
Herr_Alien <alexandru.garofide@gmail.com>
2025-07-09 13:02:47 +0300

    first render 750 ti

--- GTX750Ti.py
+++ GTX750Ti.py
@@ -6,16 +6,16 @@ configs = { "defaultAudioFile" : "GTX750Ti.ogg",\
 "benchmarkFile" : "F:\\Videos\\GTX750Ti\\Benchmark_GTX750Ti.txt",\
 "outputFolder" : "F:\\Videos\\GTX750Ti\\output", \
 "outputFile" : "GTX750Ti.mp4", \
-"textOpts" : {"fontcolor" : "White", "boxcolor" : "#80000080"},\
+"textOpts" : {"fontcolor" : "White", "boxcolor" : "#00800080"},\
 "backgroundTrack" : { "audioTracks" : [ \
-{"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "A cheap 50 Ti card", "until" : "Marvel Rivals"}}, \
-{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Marvel Rivals", "until" : "Rainbow Six: Siege"}}, \
-{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Rainbow Six: Siege", "until" : "Conclusions"}}, \
+{"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "A cheap 50 Ti card", "until" : "Games that went NOPE"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Games that went NOPE", "until" : "Control"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Control", "until" : "Conclusions"}}, \
 {"file" : "Inspired - MaikonMusic  Free Background Music  Audio Library Release.mp3", "timestamps" : ("00:00", None ), "destinationTimestamp" : {"title" : "Conclusions", "until" : "EOF"}}, \
 ], "volume" : 0.05 },\
 "episodes" : [],\
 "youtube" : {"title" : "", \
-"description" : '''In this one we re-visit the former mining superstar, AMD's RX 580.''',\
+"description" : '''In this one we see what abandoning DX11 by game devs in 2025 did to the GTX 750 Ti. Or how the baked in obsolescence by NVidia doomed an otherwise ok card. Or both.''',\
 "links" : '''
 Track: Bliss Of Heaven - SOMM [Audio Library Release]
 Music provided by Audio Library Plus
@@ -33,7 +33,7 @@ Watch: https://www.youtube.com/watch?v=RUkdTkk_52o&t=0s
 Free Download / Stream: https://alplus.io/inspired
 
 ''', \
-"tags" : "Maxwell,GTX 970,GeForce,NVidia,GTX970",\
+"tags" : "Maxwell,GTX 750 Ti,GeForce,NVidia,GTX750Ti",\
 "language" : "EN", \
 "Caption certification" : "None",\
 "recording date" : None,\
@@ -123,9 +123,8 @@ configs["episodes"].append(\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:12" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "text" : ["'Cyberpunk 2077'",\
-              scriptedvided.r6sText('' , , ),\
-              scriptedvided.r6sText('' , , ),\
-              scriptedvided.r6sText('' , , ),\
+              scriptedvided.r6sText('1280x720, low settings' , 34, 23),\
+              scriptedvided.r6sText('1280x720, low settings, FSR disabled' , 27, 18),\
 ]}, \
 })
 
@@ -134,11 +133,10 @@ configs["episodes"].append(\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:33.7" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "text" : ["'The Finals'",\
-              scriptedvided.r6sText('' , , ), \
-              scriptedvided.r6sText('' , , ), \
-              scriptedvided.r6sText('' , , ), \
+              scriptedvided.r6sText('1280x720, low settings, tutorial' , 54, 36), \
+              scriptedvided.r6sText('1280x720, low settings, match' , 52, 40), \
 ]}, \
-"video" : {"file" : ""},\
+"video" : "Discovery_2025_03_15_22_55_07_819.mp4"\
 })
 
 configs["episodes"].append(\
@@ -146,9 +144,9 @@ configs["episodes"].append(\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:57.5" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "text" : ["'Fortnite'",\
-              scriptedvided.r6sText('' , , ),\
-              scriptedvided.r6sText('' , , ),\
-              scriptedvided.r6sText('' , , ),\
+              scriptedvided.r6sText('1920x1080, performance mode' , 106, 69),\
+              scriptedvided.r6sText('1600x900, performance mode' , 139, 95),\
+              scriptedvided.r6sText('1280x720, performance mode' , 186, 107),\
 ]}, \
 })
 
@@ -156,40 +154,42 @@ configs["episodes"].append(\
 { "title": "Terminator: Resistance",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:27.5" ), "padAudio" : 0.05 },\
 "overlay" : { \
-    "benchmark" : { \
-        "settings" : "2560x1440, epic settings", \
-    }\
-}, \
+    "text" : ["'Terminator\: Resistance'",\
+              scriptedvided.r6sText('1920x1080, low settings' , 70, 53),\
+              scriptedvided.r6sText('1920x1080, medium settings' , 54, 41),\
+              scriptedvided.r6sText('1920x1080, high settings' , 42, 28),\
+]}, \
 })
 
 configs["episodes"].append(\
 { "title": "Counter-Strike 2",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:47.8" ), "padAudio" : 0.05 },\
 "overlay" : { \
-    "benchmark" : { \
-        "settings" : "1920x1080, low settings, no FSR", \
-    }\
-}, \
+    "text" : ["'Counter-Strike 2'",\
+              scriptedvided.r6sText('1920x1080, low settings' , 115, 84),\
+              scriptedvided.r6sText('1600x900, low settings' , 122, 80),\
+              scriptedvided.r6sText('1280x720, low settings' , 139, 95),\
+]}, \
 })
 
 configs["episodes"].append(\
 { "title": "Far Cry 6",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:14.2" ), "padAudio" : 0.05 },\
 "overlay" : { \
-    "benchmark" : { \
-        "settings" : "1920x1080, highest settings", \
-    }\
-}, \
+    "text" : ["'Far Cry 6'",\
+              scriptedvided.r6sText('1600x900, lowest settings' , 33, 29),\
+              scriptedvided.r6sText('1280x720, lowest settings' , 47, 39),\
+]}, \
 })
 
 configs["episodes"].append(\
 { "title": "Control",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:34.6" ), "padAudio" : 0.05 },\
 "overlay" : { \
-    "benchmark" : { \
-        "settings" : "1920x1080, high settings", \
-    }\
-}, \
+    "text" : ["'Control'",\
+              scriptedvided.r6sText('1280x720, lowest settings' , 53, 38),\
+              scriptedvided.r6sText('1280x720, low settings' , 42, 31),\
+]}, \
 })
 
 configs["episodes"].append(\
@@ -197,9 +197,9 @@ configs["episodes"].append(\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:58.2" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "text" : ["'Overwatch 2'",\
-              scriptedvided.r6sText('' , , ),\
-              scriptedvided.r6sText('' , , ),\
-              scriptedvided.r6sText('' , , ),\
+              scriptedvided.r6sText('1920x1080, low settings' , 107, 89),\
+              scriptedvided.r6sText('1600x900, low settings' , 144, 120),\
+              scriptedvided.r6sText('1280x720, low settings' , 212, 180),\
 ]}, \
 })
 
@@ -208,10 +208,10 @@ configs["episodes"].append(\
 "video" : "stock_re4demo_phoneCall.mp4", \
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:28.6" ), "padAudio" : 0.05 },\
 "overlay" : { \
-    "benchmark" : { \
-        "settings" : "1920x1080, balanced preset", \
-    }\
-}, \
+    "text" : ["'Resident Evil 4 \(Remake\)'",\
+              scriptedvided.r6sText('1280x720, prioritize performance' , 54, 29),\
+              scriptedvided.r6sText('1280x720, prioritize performance, no FSR' , 35, 21),\
+]}, \
 })
 
 configs["episodes"].append(\
@@ -229,20 +229,24 @@ configs["episodes"].append(\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:13.5" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "text" : ["'Shadow of the Tomb Raider'",\
-              scriptedvided.r6sText('' , , ),\
-              scriptedvided.r6sText('' , , ),\
+              scriptedvided.r6sText('1920x1080, lowest settings' , 39, 29),\
+              scriptedvided.r6sText('1600x900, low settings' , 38, 27),\
+              scriptedvided.r6sText('1600x900, lowest settings' , 49, 35),\
+              scriptedvided.r6sText('1280x720, medium settings' ,40 , 27),\
+              scriptedvided.r6sText('1280x720, low settings' , 48, 32),\
 ]}, \
-"video" : {"file" : " "}\
+"video" : {"file" : "SOTTR_gaming_highest.mp4", "start" : "04:53"}\
 })
 
 configs["episodes"].append(\
 { "title": "Borderlands 3",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:31.3" ), "padAudio" : 0.05 },\
 "overlay" : { \
-    "benchmark" : { \
-        "settings" : "1920x1080, badass settings", \
-    }\
-}, \
+    "text" : ["'Borderlands 3'",\
+              scriptedvided.r6sText('1920x1080, very low settings' , 40, 33),\
+              scriptedvided.r6sText('1600x900, low settings' , 42, 34),\
+              scriptedvided.r6sText('1280x720, low settings' , 43, 34),\
+]}, \
 })
 
 ####################### end of gaming section ###############################
@@ -294,7 +298,7 @@ configs["episodes"].append(\
 { "title": "RGHD",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "07:55.7" ),  "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : " "},\
+"video" : {"file" : "GTX750Ti_RGHD.mkv"},\
 })
 
 configs["episodes"].append(\
