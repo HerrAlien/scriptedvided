c7087acdac54ca9f6b14ddeacd8651cc363c5155
Herr_Alien <alexandru.garofide@gmail.com>
2025-12-07 20:40:23 +0200

    render attempt 1

--- r5_4600G_CPU.py
+++ r5_4600G_CPU.py
@@ -9,8 +9,8 @@ configs = { "defaultAudioFile" : "",\
 "textOpts" : {"fontcolor" : "White", "boxcolor" : "#80000080"},\
 "backgroundTrack" : { "audioTracks" : [ \
 {"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "Not sure about them APUs", "until" : "Marvel Rivals"}}, \
-{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Marvel Rivals", "until" : "Far Cry 6"}}, \
-{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Far Cry 6", "until" : "Video rendering test (The GigASUS)"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Marvel Rivals", "until" : "DOTA2 both"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "DOTA2 both", "until" : "Video rendering test (The GigASUS)"}}, \
 {"file" : "Ferco - Inquisitiveness.ogg", "timestamps" : ("01:01", None ), "destinationTimestamp" : {"title" : "Video rendering test (The GigASUS)", "until" : "Conclusions"}}, \
 {"file" : "Inspired - MaikonMusic  Free Background Music  Audio Library Release.mp3", "timestamps" : ("00:00", None ), "destinationTimestamp" : {"title" : "Conclusions", "until" : "EOF"}}, \
 ], "volume" : 0.038 },\
@@ -79,20 +79,26 @@ configs["episodes"].append(\
 configs["episodes"].append(\
 { "title": "The CPU",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:46.6" ), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "r5_4600G_3600_TPU_Lanes.mkv"},\
+"overlay" : { \
+    "image" : {"file" : "side_by_side_overlay.png"}\
+}, \
 })
 
 configs["episodes"].append(\
 { "title": "L3 cache compared to 3600",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:56.4" ), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "r5_4600G_3600_TPU_Cache.mkv"},\
+"overlay" : { \
+    "image" : {"file" : "side_by_side_overlay.png"}\
+}, \
 "isChapter" : False,\
 })
 
 configs["episodes"].append(\
 { "title": "TDP compared to 3600",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:03.5" ), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "R5_4600G_TechPowerup.mkv"},\
 "isChapter" : False,\
 })
 
@@ -135,7 +141,7 @@ configs["episodes"].append(\
               "'GPU\: Radeon RX 580'",\
     ]\
 }, \
-"video" : {"file" : "breel_R5_2400g_platform.mp4" and "case_final.mp4"},\
+"video" : {"file" : "breel_R5_2400g_platform.mp4"},\
 })
 
 ####################### end of intro ###############################
@@ -343,7 +349,7 @@ configs["episodes"].append(\
 "video" : {"file" : "blurred_dimmed_gigasus.mp4", "start" : "01:00"},\
 "isChapter" : False,\
 "overlay" : { \
-    "image" : {"file" : ""}\
+    "image" : {"file" : "ipc_overlay.png"}\
 }, \
 })
 
@@ -353,7 +359,7 @@ configs["episodes"].append(\
 "video" : {"file" : "blurred_dimmed_gigasus.mp4", "start" : "00:30"},\
 "isChapter" : False,\
 "overlay" : { \
-    "image" : {"file" : ""}\
+    "image" : {"file" : "ipc_all.png"}\
 }, \
 })
 
