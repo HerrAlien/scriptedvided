823a66c0b7b1a09f8041ea4c10cb7c312424e4f6
Herr_Alien <alexandru.garofide@gmail.com>
2026-01-01 15:38:26 +0200

    render 1 gt 730

--- ColorfulGT730K.py
+++ ColorfulGT730K.py
@@ -1,6 +1,6 @@
 import scriptedvided
 
-configs = { "defaultAudioFile" : ".Colorful_GT730_Kepler_DDR3.ogg",\
+configs = { "defaultAudioFile" : "Colorful_GT730_Kepler_DDR3.ogg",\
 "mediaFolder" : "F:\\Videos\\Colorful_GT730K_DDR3", \
 "stockFolder" : "F:\\Videos\\stock",\
 "benchmarkFile" : "F:\\Videos\\Colorful_GT730K_DDR3\\Benchmark_GT730_KeplerDDR3.txt",\
@@ -8,10 +8,8 @@ configs = { "defaultAudioFile" : ".Colorful_GT730_Kepler_DDR3.ogg",\
 "outputFile" : "GT730_KeplerDDR3.mp4", \
 "textOpts" : {"fontcolor" : "White", "boxcolor" : "#80000080"},\
 "backgroundTrack" : { "audioTracks" : [ \
-{"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "Potehto, potahto", "until" : "Marvel Rivals"}}, \
-{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Marvel Rivals", "until" : "DOTA2 both"}}, \
-{"file" : "Ferco - Inquisitiveness.ogg", "timestamps" : ("01:01", None ), "destinationTimestamp" : {"title" : "Terminator both", "until" : "Control"}}, \
-{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Control", "until" : "Conclusions"}}, \
+{"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "Potehto, potahto", "until" : "Cyberpunk 2077"}}, \
+{"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "Cyberpunk 2077", "until" : "Conclusions"}}, \
 {"file" : "Inspired - MaikonMusic  Free Background Music  Audio Library Release.mp3", "timestamps" : ("00:00", None ), "destinationTimestamp" : {"title" : "Conclusions", "until" : "EOF"}}, \
 ], "volume" : 0.038 },\
 "episodes" : [],\
@@ -71,41 +69,50 @@ Free Download / Stream: https://alplus.io/inspired
 
 configs["episodes"].append(\
 { "title": "Potehto, potahto",\
-"audio" : {"timestamps" : ("00:00", ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : ("00:00", "00:17"), "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : "breel_ColorfulGt730K_alone2_barred.mp4"},\
 })
 
 # GPU-Z
 configs["episodes"].append(\
 { "title": "The GPU",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:32" ), "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : "Colorful_GT730_GPUZ.mkv"},\
 })
 
 configs["episodes"].append(\
 { "title": "Copmarisons Fermi",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:49.1" ), "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : "breel_FermiGT730_outside.mp4"},\
+"overlay" : { \
+    "image" : {"file" : "Family_overlay_Fermi.png"}\
+}, \
 "isChapter" : False,\
 })
 
 configs["episodes"].append(\
 { "title": "Copmarisons Kepler GDDR5",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : " "},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:09.1" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "Zotac_GT730_GDDR5.mkv", "start" : "00:30"},\
+"overlay" : { \
+    "image" : {"file" : "Family_overlay_Fermi_Kepler_GDDR5.png"}\
+}, \
 "isChapter" : False,\
 })
 
 configs["episodes"].append(\
 { "title": "Copmarisons Kepler DDR3",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:22.9" ), "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : "breel_ColorfulGt730K_lookingAround_barred.mp4"},\
+"overlay" : { \
+    "image" : {"file" : "Family_overlay_all.png"}\
+}, \
 "isChapter" : False,\
 })
 
 configs["episodes"].append(\
 { "title": "Cooling and thermals",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:41.5" ), "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : "breel_ColorfulGt730K_cooling2_barred.mp4"},\
 "overlay" : { \
     "text" : ["'Temperatures (Valley)\: 39C (16C delta over ambient)'"]}, \
@@ -113,7 +120,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "The test system",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:56.2" ), "volume" : 0.999, "padAudio" : 0.05 },\
 "overlay" : { \
     "text" : ["'Custom PC'",\
               "'CPU\: Ryzen 5 5600'",\
@@ -130,7 +137,7 @@ configs["episodes"].append(\
 # needs redone
 configs["episodes"].append(\
 { "title": "Cyberpunk 2077",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:18" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1280x720, lowest settings", \
@@ -141,7 +148,7 @@ configs["episodes"].append(\
 # single resolution
 configs["episodes"].append(\
 { "title": "The Finals",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:30.7" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1280x720, lowest settings", \
@@ -151,7 +158,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Overwatch 2",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:46.6" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "image" : {"file" : "Overwatch 2, 1280x720, low settings.png"}\
 }, \
@@ -159,7 +166,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Fortnite",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:10.9" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "image" : {"file" : "Fortnite, 1280x720, 67% render scale, performance mode.png"}\
 }, \
@@ -168,7 +175,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Terminator",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:26.1" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "image" : {"file" : "Terminator, 1024x768, low settings.png"}\
 }, \
@@ -176,15 +183,25 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Counter-Strike 2",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:51.4" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "image" : {"file" : "Counter-Strike 2, 1024x768, balanced FSR, low settings.png"}\
 }, \
 })
 
 configs["episodes"].append(\
+{ "title": "Doom Eternal",\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:07.4" ), "padAudio" : 0.05 },\
+"overlay" : { \
+    "benchmark" : { \
+        "settings" : "1280x720, low settings", \
+    }\
+}, \
+})
+
+configs["episodes"].append(\
 { "title": "Rainbow Six: Siege",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:22.3"), "padAudio" : 0.05 },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1280x720, lowest settings", \
@@ -195,7 +212,7 @@ configs["episodes"].append(\
 # single resolution
 configs["episodes"].append(\
 { "title": "Control",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:39.6" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "image" : {"file" : "Control, 1280x720, lowest settings.png"}\
 }, \
@@ -204,7 +221,7 @@ configs["episodes"].append(\
 # single resolution
 configs["episodes"].append(\
 { "title": "DOTA 2",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:57.4" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "image" : {"file" : "DOTA2, 1920x1080, low settings.png"}\
 }, \
@@ -215,20 +232,20 @@ configs["episodes"].append(\
 #redo and specify a smoother video
 configs["episodes"].append(\
 { "title": "Shadow of the Tomb Raider",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:16.1" ), "padAudio" : 0.05 },\
 "overlay" : { \
     "image" : {"file" : "Shadow of the Tomb Raider, 1280x720, 50% scale, lowest settings.png"}\
 }, \
 "video" : {"file" : "SOTTR_gaming_highest.mp4", "start" : "04:53"}\
 })
 
-configs["episodes"].append(\
-{ "title": "Borderlands 3",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
-"overlay" : { \
-    "image" : {"file" : "Borderlands 3, 1280x720, 50% render scale, very low settings.png"}\
-}, \
-})
+#configs["episodes"].append(\
+#{ "title": "Borderlands 3",\
+#"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "padAudio" : 0.05 },\
+#"overlay" : { \
+#    "image" : {"file" : "Borderlands 3, 1280x720, 50% render scale, very low settings.png"}\
+#}, \
+#})
 
 ####################### end of gaming section ###############################
 
@@ -237,16 +254,13 @@ configs["episodes"].append(\
 # sum of all fps
 configs["episodes"].append(\
 { "title": "Conclusions",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ),  "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : " "},\
-"overlay" : { \
-    "image" : {"file" : "breel_ColorfulGt730K_alone_barred.mp4"}\
-}, \
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:32.7" ),  "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : "breel_ColorfulGt730K_alone_barred.mp4"},\
 })
 
 configs["episodes"].append(\
 { "title": "GTAV",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:47.2"), "padAudio" : 0.05 },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1280x720, lowest settings", \
@@ -256,7 +270,7 @@ configs["episodes"].append(\
 
 configs["episodes"].append(\
 { "title": "Warframe",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:03"), "padAudio" : 0.05 },\
 "overlay" : { \
     "benchmark" : { \
         "settings" : "1280x720, lowest settings", \
@@ -265,16 +279,23 @@ configs["episodes"].append(\
 })
 
 configs["episodes"].append(\
-{ "title": "Pricing",\
+{ "title": "Not for gaming",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ),  "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:13.5" ),  "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : "breel_ColorfulGt730K_alone2_barred.mp4"},\
+})
+
+configs["episodes"].append(\
+{ "title": "OLX pricing",\
+"isChapter" : False,\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:22.9" ),  "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "bye",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ),  "volume" : 0.999, "padAudio" : 0.05 },\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:31.3" ),  "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : "breel_ColorfulGt730K_lookingAround_barred.mp4"},\
 })
 
