1c5aa576b75f3e4b225f7f785d9f7da8fa87c791
Hindsight_Lad <alexandru.garofide@siemens.com>
2026-05-25 12:15:45 +0300

    cleaned x570

--- TUF_X570_GamingWiFi.py
+++ TUF_X570_GamingWiFi.py
@@ -9,7 +9,7 @@ configs = { "defaultAudioFile" : "   . . . .  ",\
 "outputFile" : "   . . . .  ", \
 "textOpts" : {"fontcolor" : "White", "boxcolor" : "#80000080"},\
 "backgroundTrack" : { "audioTracks" : [ \
-{"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "Better than most", "until" : "The VRMs"}}, \
+{"file" : "Bliss Of Heaven - SOMM [Audio Library Release]-Free Copyright-safe Music.mp3", "timestamps" : ("00:20", None ), "destinationTimestamp" : {"title" : "Best board that I sold", "until" : "The VRMs"}}, \
 {"file" : "Far Far Away - Ferco _ Free Background Music _ Audio Library Release.mp3", "timestamps" : ("00:33", None ), "destinationTimestamp" : {"title" : "The VRMs", "until" : "Audio sample"}}, \
 {"file" : "Ferco - Inquisitiveness.ogg", "timestamps" : ("01:01", None ), "destinationTimestamp" : {"title" : "The BIOS setup utility", "until" : "Conclusions"}}, \
 {"file" : "Inspired - MaikonMusic  Free Background Music  Audio Library Release.mp3", "timestamps" : ("00:00", None ), "destinationTimestamp" : {"title" : "Conclusions", "until" : "EOF"}}, \
@@ -70,294 +70,294 @@ TechPowerup entries: https://www.techpowerup.com/gpu-specs/asus-r7-260-1-gb.b273
 #configs["episodes"].append(\
 #{ "title": "A favorite of miners and gamers alike",\
 #"audio" : {"timestamps" : ("09:37.3", "09:46" ), "volume" : 0.999, "padAudio" : 0.05 },\
-#"video" : {"file" : " "},\
+#"video" : {"file" : ""},\
 #})
 
 configs["episodes"].append(\
 { "title": "Best board that I sold",\
-"audio" : {"timestamps" : ("00:00", "00:23.9" ), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Overview_bright.mp4"},\
+"audio" : {"timestamps" : ("00:00", "" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "The VRMs",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:37.6"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_VRM_Heatsink_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "VRMs - core",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:44.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_VRM_Core_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "VRMs - soc",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:00"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_VRM_SOC_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "EPS",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:00"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_VRM_SOC_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "4 DIMM slots, 4 clips",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:09.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_DIMMs_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 
 configs["episodes"].append(\
 { "title": "Expansion slots, ports and headers",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:28.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Slots_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "plenty of room under the main x16",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:37"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Slots_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "M.2 slots maybe with arrow overlay",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:46.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Slots_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "SATA ports",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:57.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Pins_USB2_FP_SATA_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 
 configs["episodes"].append(\
 { "title": "Debug LEDs",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:57.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Pins_USB2_FP_SATA_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Pin - audio",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:08.2"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Pins_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Pin - usb2, fans, front panel, clear cmos",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:15.5"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_RGB_USB2_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Pin USB3",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:35.2"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_PinsUsb3_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Pin RGB",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:44.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Pins_Rgb_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "CPU fan pins",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:55.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Pins_Tpm_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "SPI header",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:55.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Pins_Tpm_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 # too long of a cut ...
 configs["episodes"].append(\
 { "title": "Rear IO",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:18.3"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_IO_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 }) # maybe an overlay with the DVI-D to HDMI adapter?
 
 # too long of a cut ...
 configs["episodes"].append(\
 { "title": "with router antenna",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:18.3"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_IO_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 "isChapter" : False,\
 }) # maybe an overlay with the DVI-D to HDMI adapter?
 
 configs["episodes"].append(\
 { "title": "Audio introduction",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:21.7" ), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "Prime_B550m-a-WiFi_audio.mkv"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "" ), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Audio sample",\
 "isChapter" : False,\
 "audio" : {"timestamps" : ("03:07.7", "03:21.7" ), "volume" : 0.001, "padAudio" : 0.05 },\
-"video" : {"file" : "Prime_B550m-a-WiFi_audio.mkv"},\
+"video" : {"file" : ""},\
 })
 
 # EC spi chip first
 configs["episodes"].append(\
 { "title": "The BIOS setup utility",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:31.6"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BiosEz.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 }) # list of bios
 
 # maybe side by 
 configs["episodes"].append(\
 { "title": "BIOS advanced, CPU freq",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:43.5"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_CpuFreq.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "CPU voltage",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:02.9"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_CpuCoreVoltage.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Mem profile - maybe both Easy and advanced",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:09.6"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_DOCP.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Mem subtimings",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:20.1"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_RamManual.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "REBAR and TPM",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:28.2"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "Prime_B550m-a-wifi-tpm-rebar.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Fans types",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:45.2"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_FanTypeAndCurve.MP4" , "start" : "00:11"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "fan curve",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:51.6"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_FanCurveEditor.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Tools - ezflash",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:00.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_Tools.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Tools - oc profiles",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:00.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_Tools.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Tools - AURA",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:00.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_Tools.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 # likes
 configs["episodes"].append(\
 { "title": "Conclusions",\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:09.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Overview_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "has wifi",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:14.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_VRM_Heatsink_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "likes - vrm for 200W plus",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:16.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "Prime_B550m-a-WiFi_audio.mkv"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "but I do not use more than 100W",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:20.9"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Slots_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "HP Z230",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:29.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550M-A_WiFi_BIOS_FanTypeAndCurve.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "I test old GPUs, B550 at most for me",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:47.3"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Overview_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "x570 is wasted on me",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:55"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "TufX570Gaming+Wifi_overview.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "other boards",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:55"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "TufX570Gaming+Wifi_overview.MP4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 configs["episodes"].append(\
 { "title": "Bye",\
 "isChapter" : False,\
-"audio" : {"timestamps" : (scriptedvided.nextTS(configs), "06:03.9"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : "PrimeB550-A_WiFi_Overview_bright.mp4"},\
+"audio" : {"timestamps" : (scriptedvided.nextTS(configs), ""), "volume" : 0.999, "padAudio" : 0.05 },\
+"video" : {"file" : ""},\
 })
 
 scriptedvided.makeVideo(configs)
