8750bf1afa9ebdd3349cd2c90605d18044a42333
Herr_Alien <alexandru.garofide@gmail.com>
2026-06-28 23:59:30 +0300

    add a few videos

--- Huananzhi_x99_qd4.py
+++ Huananzhi_x99_qd4.py
@@ -76,7 +76,7 @@ TechPowerup entries: https://www.techpowerup.com/gpu-specs/asus-r7-260-1-gb.b273
 configs["episodes"].append(\
 { "title": "X99 ... not!",\
 "audio" : {"timestamps" : ("00:00", "00:12.2" ), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_overview.MP4"},\
 })
 
 configs["episodes"].append(\
@@ -84,52 +84,53 @@ configs["episodes"].append(\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:20.7"), "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : ""},\
-}) # add overlay
+}) # add the CPUZ as overlay 
 
 
 
 configs["episodes"].append(\
 { "title": "The VRMs",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:27.3"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_VRM.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "VRMs - core",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:39.3"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "Huananzhi_x99_qd4_VrmMosfets2_barred.mp4"},\
 "overlay" : {"image" : {"file" : ""} }, \
 }) # add overlay
 
 configs["episodes"].append(\
 { "title": "4 DIMM slots, 8 clips",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "00:55.6"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_DIMMs.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "quad channel",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:09.6"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiX99QD4_E5-2670-v3_RAM-good_barred.mp4"},\
 "overlay" : {"image" : {"file" : ""} }, \
-}) # add overlay
+}) # add overlay qith cpuz again
+# HuananzhiX99QD4_E5-2670-v3_RAM-good_barred.mp4 or E5-2670-v3_LargerCPU-Z_mem.mkv, but trimmed down
 
 
 configs["episodes"].append(\
 { "title": "Expansion slots, ports and headers",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:22.6"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_slots.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "M.2 slots maybe with arrow overlay",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:32.4"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_slots.MP4"},\
 "overlay" : {"image" : {"file" : ""} }, \
-})
+}) # maybe overlay with the arrows pushing to the CPU?
 
 configs["episodes"].append(\
 { "title": "q87 and SATA channels",\
@@ -137,78 +138,78 @@ configs["episodes"].append(\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:40.3"), "volume" : 0.999, "padAudio" : 0.05 },\
 "video" : {"file" : ""},\
 "overlay" : {"image" : {"file" : ""} }, \
-})
+})  # overlay with Intel Ark for Q87 SATA 
 
 configs["episodes"].append(\
 { "title": "Xeon and available PCIE lanes",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:47"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "breel_E5-2670-v3_mobo_inHand_barred.mp4"},\
 "overlay" : {"image" : {"file" : ""} }, \
-})
+}) # overlay with Intel Ark for Xeon PCIE lanes 
 
 
 configs["episodes"].append(\
 { "title": "SATA ports",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "01:59.3"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_SATA.MP4"},\
 }) # ... maybe anoverlay with the q87 and available sata channels?
 
 configs["episodes"].append(\
 { "title": "Pin - audio, COM",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:14.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_pinsUpToUSB3.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "Pin - USBs",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:19.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_pins2.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "clear CMOS",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:25.1"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_pins2.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "3 pin fan header",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:29.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_pins2.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "front panel connector",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:34.4"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_pins2.MP4"},\
 })
 
 # too long of a cut ...
 configs["episodes"].append(\
 { "title": "Rear IO",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:41.3"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_IO.MP4"},\
 }) # maybe an overlay with the DVI-D to HDMI adapter?
 
 # too long of a cut ...
 configs["episodes"].append(\
 { "title": "Maybe overlay with Xeons and no IGPU?",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:47.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "breel_E5-2670-v3_mobo_inHand_barred.mp4"},\
 "isChapter" : False,\
-})
+}) # picture overlay with the Xeon ARC page
 
 configs["episodes"].append(\
 { "title": "Other rear io",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "02:57.9"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_IO.MP4"},\
 "isChapter" : False,\
 })
 
@@ -224,70 +225,70 @@ configs["episodes"].append(\
 { "title": "Audio conclusions",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:02.4" ), "volume" : 0.001, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "Huananzhi-X99-QD4-BIOS-OC-bad.MP4"},\
 })
 
 # EC spi chip first
 configs["episodes"].append(\
 { "title": "The BIOS setup utility",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:10.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "Huananzhi-X99-QD4-BIOS-CpuFreq.MP4"},\
 }) # use the core clock already
 
 configs["episodes"].append(\
 { "title": "CPU voltage",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:19.4"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "Huananzhi-X99-QD4-BIOS-VCore.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "Power limits",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:31.8"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "Huananzhi-X99-QD4-BIOS-Power.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "REBAR-ish",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:37.3"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "Huananzhi-X99-QD4-BIOS-Rebar.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "CSM requirements",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:44.4"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "Huananzhi-X99-QD4-BIOS-Rebar.MP4", "start" : "00:00"},\
 })
 
 # likes
 configs["episodes"].append(\
 { "title": "Conclusions",\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "03:55.5"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_2023.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "not the best VRM",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:02.7"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "Huananzhi_x99_qd4_VrmMosfets2_barred.mp4"},\
 })
 
 configs["episodes"].append(\
 { "title": "one slot per mem channel",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:09"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiX99QD4_E5-2670-v3_RAM-good_barred.mp4"},\
 })
 
 configs["episodes"].append(\
 { "title": "RDIMM vs UDIMM",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:19"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "ddr4_Rdimm_vs_Udimm_barred.mp4"},\
 })
 
 configs["episodes"].append(\
@@ -301,7 +302,7 @@ configs["episodes"].append(\
 { "title": "Pricing breel",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "04:35.2"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_2023.MP4"},\
 })
 
 configs["episodes"].append(\
@@ -329,14 +330,14 @@ configs["episodes"].append(\
 { "title": "not really recommended",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:06.2"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "HuananzhiQD4_2023.MP4"},\
 })
 
 configs["episodes"].append(\
 { "title": "zoom on CPUs",\
 "isChapter" : False,\
 "audio" : {"timestamps" : (scriptedvided.nextTS(configs), "05:15.9"), "volume" : 0.999, "padAudio" : 0.05 },\
-"video" : {"file" : ""},\
+"video" : {"file" : "breel_E5-2670-v3_mobo_inHand_barred.mp4"},\
 })
 
 configs["episodes"].append(\
